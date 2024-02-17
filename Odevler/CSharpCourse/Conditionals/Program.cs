4d2669c92e42ee496e3ab4ecd00ab1e7e7428b3c
Sueda Turgut <suedat24@gmail.com>
2024-02-17 23:35:15 +0300

    conditionals eklendi

---
+++ Odevler/CSharpCourse/Conditionals/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Conditionals
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            var number = 10;
+            if (number == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10");
+            }
+
+            //Single Line If
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
