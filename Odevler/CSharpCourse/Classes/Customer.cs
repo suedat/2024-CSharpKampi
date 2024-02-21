78488d494d16b1072df873412074b33f63bbc4f6
Sueda Turgut <suedat24@gmail.com>
2024-02-22 02:21:09 +0300

    classes eklendi

---
+++ Odevler/CSharpCourse/Classes/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Classes
+{
+	class Customer
+	{
+
+		//field
+		private string _firstName;
+
+		//property
+		public int Id { get; set; }
+		public string FirstName{
+			get { return "Mrs." + _firstName; }
+			set { _firstName = value; }
+		}
+		public string LastName { get; set; }
+	}
+}
+
---
+++ Odevler/CSharpCourse/Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Update();
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add();
+        productManager.Update();
+
+        Customer customer = new Customer();
+        customer.Id = 1;
+        customer.FirstName = "Engin";
+        customer.LastName = "Demiroğ";
+
+        Customer customer2 = new Customer
+        {
+            Id = 2, FirstName = "Derin", LastName = "Demiroğ"
+        };
+
+        Console.ReadLine();
+    }
+}
